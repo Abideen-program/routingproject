e6d0ab97f0feef3ed3d6f378e1af2584c0c3111a
Abideen <olafimihana@gmail.com>
2023-03-08 21:30:29 +0530

    created links to all the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,33 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Navigation from './components/Naviagtion/Navigation';
+import EditEventPage from './components/routes/EditEventPage/EditEventPage';
+import EventDetailPage from './components/routes/EventDetailPage/EventDetailPage';
+import EventPage from './components/routes/EventPage/EventPage';
+import HomePage from './components/routes/HomePage/HomePage';
+import NewEventPage from './components/routes/NewEventPage/NewEventPage';
+
 function App() {
-  return <div></div>;
+
+  const router = createBrowserRouter([
+    {
+      path: '/', 
+      element: <Navigation />,
+      children: [
+        {index: true, element: <HomePage />},
+        {path: '/events', element: <EventPage />},
+        {path: '/events/:id', element: <EventDetailPage />},
+        {path: '/events/new', element: <NewEventPage />},
+        {path: '/events/:id/edit', element: <EditEventPage />}
+      ]
+    }
+  ])
+
+  return (
+      <RouterProvider router={router} />
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Naviagtion/Navigation.jsx
@@ -0,0 +1,53 @@
+import { NavLink, Outlet } from "react-router-dom";
+import classes from "./Navigation.module.css";
+const Navigation = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <nav>
+          <ul className={classes.list}>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                to="/"
+                end
+              >
+                HOME
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                to="/events"
+                end
+              >
+                EVENT
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                to="/events/new"
+              >
+                NEW EVENT
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Navigation;
