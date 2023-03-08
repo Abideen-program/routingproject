1ab46a60a943f36ffa6166307c5e1a4831dc4938
Abideen <olafimihana@gmail.com>
2023-03-08 22:43:46 +0530

    setting up route to the event detail page

--- frontend/src/components/routes/EventDetailPage/EventDetailPage.jsx
+++ frontend/src/components/routes/EventDetailPage/EventDetailPage.jsx
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
 const EventDetailPage = () => {
+  const params = useParams()
   return (
     <>
       <h1>Event detail page</h1>
+      <p>{params.id}</p>
+      <Link to='..' relative="path">Back</Link>
     </>
   );
 };
--- frontend/src/components/routes/EventPage/EventPage.jsx
+++ frontend/src/components/routes/EventPage/EventPage.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 const EventPage = () => {
+  const EVENTS = [
+    { id: "e1", title: "Go to market" },
+    { id: "e2", title: "Go to School" },
+    { id: "e3", title: "Go to House" },
+    { id: "e4", title: "Go to Mosque" },
+  ];
   return (
     <>
       <h1>Event page</h1>
+      <ul>
+        {EVENTS.map((event) => {
+          return (
+            <li key={event.id}>
+              <Link to={`/events/${event.id}`}>{event.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
